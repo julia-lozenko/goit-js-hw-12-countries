89fd98a55f333305292c2aabf275da932e77c763
julia-lozenko <jlozenko@gmail.com>
2021-05-23 17:20:44 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import fetchCountries from './api/fetchCountries';
-import temp from './templates/country.hbs';
+import singleCountry from './templates/country.hbs';
 import countriesList from './templates/countries-list.hbs';
 
 const debounce = require('lodash.debounce');
@@ -15,7 +15,7 @@ function renderCountriesHere(data) {
   console.log(data)
   countryRef.innerHTML = '';
   if (data.length === 1) {
-    const markUp = temp(data[0]);
+    const markUp = singleCountry(data[0]);
     countryRef.insertAdjacentHTML('afterbegin', markUp);
   } else if (data.length > 1 && data.length <= 10) {
     const markUp = countriesList(data);
@@ -29,7 +29,5 @@ function searchHandler(e) {
     .then(data => renderCountriesHere(data))
     .catch(err => {
       console.log(`error: ${err.response}`);
-      renderCountries.toError();
     });
 }
-
